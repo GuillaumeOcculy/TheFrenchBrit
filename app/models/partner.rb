ca572c24ec949d5de02826f0e4b14a692a81f798
Guillaume Occuly <occuly.guillaume@gmail.com>
2015-07-14 16:34:09 +0200

    Image bigger

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -7,12 +7,12 @@ class Partner < ActiveRecord::Base
   # Scopes
   scope :recent, -> { order('created_at desc')}
   scope :priority, -> { order('priority asc')}
-  
+
   # Validations
   validates_presence_of :name
   validates_presence_of :avatar
 
   # Paperclip
-  has_attached_file :avatar, :styles => { large: "500x500>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { mega: "800x800>", large: "500x500>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
